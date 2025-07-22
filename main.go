5631f115d08c60ccb00d69687f3703a794276da2
Evgeniy Zherzdev <zherzdev.en@sibvaleo.com>
2025-07-22 10:58:16 +0300

    Solved

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
-import (
-  "fmt"
-)
-
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+import "fmt"
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
+	array := []int{4, 2, 4, 5, 6}
+	fmt.Println(maximumUniqueSubarray(array))
+}
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+func maximumUniqueSubarray(nums []int) int {
+	seen := make(map[int]bool)
+	left := 0
+	sum, maxSum := 0, 0
+
+	for right := 0; right < len(nums); right++ {
+		for seen[nums[right]] {
+			seen[nums[left]] = false
+			sum -= nums[left]
+			left++
+		}
+		seen[nums[right]] = true
+		sum += nums[right]
+
+		if sum > maxSum {
+			maxSum = sum
+		}
+	}
+
+	return maxSum
 }
